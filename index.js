397c91d2244226d76054c136f8a66c1f58386852
edmundo papi <edmundopierre@yahoo.com>
2020-12-23 21:58:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ function fetchBooks() {
 
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks(
-
-    function fetchBooks() {
-	  fetch('https://anapioficeandfire.com/api/books')
-    .then(resp => resp.json())
-    .then(json => renderBooks(json))
-
-}
-
-  )
-}
-)
+// function renderBooks(books) {
+//   const main = document.querySelector('main')
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2')
+//     h2.innerHTML = book.name
+//     main.appendChild(h2)
+//   })
+// }
+// 
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks(
+// 
+//     function fetchBooks() {
+// 	  fetch('https://anapioficeandfire.com/api/books')
+//     .then(resp => resp.json())
+//     .then(json => renderBooks(json))
+// 
+// }
+// 
+//   )
+// }
+// )
